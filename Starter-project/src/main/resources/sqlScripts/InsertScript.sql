91db825c7b911441bf96a54736319aa5661e43aa
Damjan <Damjan@DESKTOP-CS9964O>
2021-04-12 00:50:36 +0200

    3

---
+++ Starter-project/src/main/resources/sqlScripts/InsertScript.sql
@@ -0,0 +1,51 @@
+INSERT INTO obrazovanje
+	VALUES(1,'ftn iis','oas','diplomirani inzenjer informacionih sistema');
+INSERT INTO obrazovanje
+	VALUES(2,'ftn sao','oas','diplomirani inzenjer saobracaja');
+INSERT INTO obrazovanje
+	VALUES(3,'pmf mat','oas','diplomirani profesor matematike');
+INSERT INTO obrazovanje
+	VALUES(4, 'gimanzija','srednja sk','prirodno-matematicki smer');
+	
+	
+INSERT INTO preduzece
+	VALUES(1,'preduzece 1',31412,'novi sad','fino preduzece');
+INSERT INTO preduzece
+	VALUES(2,'preduzece 2',12313,'backi petrovac','a onako sta ja znam moze to bolje');
+INSERT INTO preduzece
+	VALUES(3,'preduzece 3',53454,'zrenjanin','bolje ista nego nista');
+INSERT INTO preduzece
+	VALUES(4,'preduzece 4',13123,'beograd','bolje nista nego ovo');
+	
+INSERT INTO radnik
+	VALUES(1,'Jovan','Kornjaca',007112,2,1);
+INSERT INTO radnik
+	VALUES(2,'Pedja','Hrkljus',007683,4,2);
+INSERT INTO radnik
+	VALUES(3,'Milica','Njegovan',007114,3,3);
+INSERT INTO radnik
+	VALUES(4,'Milivoj','Bezdan',007541,1,4);
+	
+	
+INSERT INTO sektor
+	VALUES(1,'prvi sektor','prv',1);
+INSERT INTO sektor
+	VALUES(2,'drugi sektor','drg',2);
+INSERT INTO sektor
+	VALUES(3,'treci sektor','trc',3);
+INSERT INTO sektor
+	VALUES(4,'cetvrti sektor','cet',4);
+	
+INSERT INTO obrazovanje
+	VALUES(-100, 'test naz','test oz','test opis');
+INSERT INTO preduzece
+	VALUES(-100, 'test naz','100','test lok','test opis');
+INSERT INTO radnik
+	VALUES(-100, 'test','testic',100,1,1);
+INSERT INTO sektor
+	VALUES(-100, 'test sektor','tes',1);
+	
+
+	
+	
+	
