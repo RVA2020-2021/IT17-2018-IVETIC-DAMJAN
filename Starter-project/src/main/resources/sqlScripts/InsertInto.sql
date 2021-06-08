699109dae753dcdde390c89d8b3eccf14908c1b6
damjanivetic <damjanivetic@uns.ac.rs>
2021-06-08 16:09:16 +0200

    final

--- Starter-project/src/main/resources/sqlScripts/InsertInto.sql
+++ Starter-project/src/main/resources/sqlScripts/InsertInto.sql
@@ -1,39 +1,39 @@
 INSERT INTO obrazovanje
-	VALUES(1,'ftn iis','oas','diplomirani inzenjer informacionih sistema');
+	VALUES(nextval('obrazovanje_seq'),'ftn iis','oas','diplomirani inzenjer informacionih sistema');
 INSERT INTO obrazovanje
-	VALUES(2,'ftn sao','oas','diplomirani inzenjer saobracaja');
+	VALUES(nextval('obrazovanje_seq'),'ftn sao','oas','diplomirani inzenjer saobracaja');
 INSERT INTO obrazovanje
-	VALUES(3,'pmf mat','oas','diplomirani profesor matematike');
+	VALUES(nextval('obrazovanje_seq'),'pmf mat','oas','diplomirani profesor matematike');
 INSERT INTO obrazovanje
-	VALUES(4, 'gimanzija','srednja sk','prirodno-matematicki smer');
+	VALUES(nextval('obrazovanje_seq'), 'gimanzija','srednja sk','prirodno-matematicki smer');
 	
 	
 INSERT INTO preduzece
-	VALUES(1,'preduzece 1', 'fino preduzece',31412, 'novi sad');
+	VALUES(nextval('preduzece_seq'),'preduzece 1', 'fino preduzece',31412, 'novi sad');
 INSERT INTO preduzece
-	VALUES(2,'preduzece 2','a onako sta ja znam moze to bolje' ,12313,'backi petrovac');
+	VALUES(nextval('preduzece_seq'),'preduzece 2','a onako sta ja znam moze to bolje' ,12313,'backi petrovac');
 INSERT INTO preduzece
-	VALUES(3,'preduzece 3','bolje ista nego nista' ,53454,'zrenjanin');
+	VALUES(nextval('preduzece_seq'),'preduzece 3','bolje ista nego nista' ,53454,'zrenjanin');
 INSERT INTO preduzece
-	VALUES(4,'preduzece 4','bolje nista nego ovo' ,13123,'beograd');
+	VALUES(nextval('preduzece_seq'),'preduzece 4','bolje nista nego ovo' ,13123,'beograd');
 	
 INSERT INTO sektor
-	VALUES(1,'prvi sektor','prv',1);
+	VALUES(nextval('sektor_seq'),'prvi sektor','prv',1);
 INSERT INTO sektor
-	VALUES(2,'drugi sektor','drg',2);
+	VALUES(nextval('sektor_seq'),'drugi sektor','drg',2);
 INSERT INTO sektor
-	VALUES(3,'treci sektor','trc',3);
+	VALUES(nextval('sektor_seq'),'treci sektor','trc',3);
 INSERT INTO sektor
-	VALUES(4,'cetvrti sektor','cet',4);
+	VALUES(nextval('sektor_seq'),'cetvrti sektor','cet',4);
 	
 INSERT INTO radnik
-	VALUES(1,007112,'Jovan','Kornjaca',2,1);
+	VALUES(nextval('radnik_seq'),007112,'Jovan','Kornjaca',2,1);
 INSERT INTO radnik
-	VALUES(2,007683,'Pedja','Hrkljus',4,2);
+	VALUES(nextval('radnik_seq'),007683,'Pedja','Hrkljus',4,2);
 INSERT INTO radnik
-	VALUES(3,007114,'Milica','Njegovan',3,3);
+	VALUES(nextval('radnik_seq'),007114,'Milica','Njegovan',3,3);
 INSERT INTO radnik
-	VALUES(4,007541,'Milivoj','Bezdan',1,4);
+	VALUES(nextval('radnik_seq'),007541,'Milivoj','Bezdan',1,4);
 	
 	
 
